90e304a4c553666a15180a614ef0d147f8c57387
porimol <p.c_roy@yahoo.com>
2015-11-22 23:12:00 +0600

    data structure problem solving init

---
+++  DataStructures/arrays-ds.cpp
@@ -0,0 +1,24 @@
+/*
+/* Solved By : Porimol
+/* Date : 22-11-2015
+/* Problem : https://www.hackerrank.com/challenges/arrays-ds
+/* Status : Accepted
+*/
+
+
+#include <stdio.h>
+
+int main() {
+
+    int n, i, j, temp, arr[1000];
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for(i = n-1; i >= 0; i--)
+       printf("%d ", arr[i]);
+
+    return 0;
+}
